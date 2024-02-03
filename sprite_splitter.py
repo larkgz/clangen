07d523a5286cce734c0e8a974d036a96e1a5cc96
Lara <63805048+larkgz@users.noreply.github.com>
2024-02-03 14:20:30 -0500

    feat: Remove original files

--- sprite_splitter.py
+++ sprite_splitter.py
@@ -6,6 +6,7 @@ Tool to split Clangen sprites into separate files.
 """
 
 import os
+import glob
 
 # set default directory for MacOS
 directory = os.path.dirname(__file__)
@@ -17,3 +18,12 @@ os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
 from scripts.cat.sprites import sprites
 sprites.load_all()
+
+for png in glob.glob("sprites/*.png"):
+    os.remove(png)
+for png in glob.glob("sprites/faded/*.png"):
+    os.remove(png)
+os.rmdir("sprites/faded")
+for png in glob.glob("sprites/paralyzed/*.png"):
+    os.remove(png)
+os.rmdir("sprites/paralyzed")
